25ad49a86ccb4a3cb4250ed544bd1a5dec5b0a2d
Go Nakano <g.nakano555@gmail.com>
2019-04-28 14:10:06 +0900

    update test file, add fixtures file

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
+const request = require('supertest');
+const expect = require('chai').expect;
 const knex = require('../db/knex');
+const app = require('../app');
+const fixtures = require('./fixtures');
 
 describe('CRUD drums', () => {
-  before(() => {
-    return knex.migrate.latest()
+  before((done) => {
+    knex.migrate.latest()
       .then(() => {
         return knex.seed.run();
+      }).then(() => done());
+  });
+  
+  it('lists all records', (done) => {
+    request(app)
+      .get('/api/v1/drums')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('array');
+        expect(response.body).to.deep.equal(fixtures.drums);
+        done();
       });
-    });
+  });
 });
